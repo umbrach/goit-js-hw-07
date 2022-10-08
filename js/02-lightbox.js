876ed441fdfdf227fefded80b41f890e6d115980
Denys Barabanov <umbrach415@gmail.com>
2022-10-09 00:27:38 +0300

    Create gallery-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsArray = [];
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryItemsArray.push(
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  );
+});
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsArray.join(""));
+
+// ----------------------------
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
