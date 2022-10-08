709370ad86fcdf30bbdab6bd2c29f319daf8d1a3
Denys Barabanov <umbrach415@gmail.com>
2022-10-09 00:27:24 +0300

    Create gallery-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsArray = [];
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryItemsArray.push(
+    `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+  );
+});
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsArray.join(""));
+
+// ---------------------------------
+
+
+let instance;
+const instanceOpen = event => {
+    event.preventDefault();
+    instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    if (event.target === event.currentTarget) {
+        return
+    }
+    instance.show();
+    galleryEl.addEventListener('keydown', instanceClose)
+}
+// ---------------------------------
+
+const instanceClose = (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+};
+
+// ---------------------------------
+
+galleryEl.addEventListener('click', instanceOpen)
